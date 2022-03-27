cd8a4ec07c184fedb20aeb023fb2a21a0fc9785d
Alex <alex@totallynotmyemail.com>
2022-03-27 01:50:16 +0100

    Fixed spreading
    Signed-off-by: Alex <alex@totallynotmyemail.com>

--- constants/healthConstants.go
+++ constants/healthConstants.go
@@ -1,1 +1,15 @@
 package constants
+
+const KBaseTimeUntilIncubation = 1
+const KVarianceInTimeUntilIncubation = 0
+
+const KBaseTimeUntilInfection = 3
+const KVarianceInTimeUntilInfection = 1
+
+const KBaseTimeUntilRecovery = 7
+const KVarianceInTimeUntilRecovery = 3
+
+const KBaseTimeUntilProtectionAfterCovidOver = 200
+const KVarianceInTimeUntilProtectionAfterCovidOver = 100
+
+const KProbabilityOfInfection = 0.0006
