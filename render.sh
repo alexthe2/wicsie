36bf5dcefaf6f3773eafe9986540c56ce681ff57
Alex <alex@totallynotmyemail.com>
2022-03-27 14:23:09 +0200

    render s
    Signed-off-by: Alex <alex@totallynotmyemail.com>

--- render.sh
+++ render.sh
@@ -1,1 +1,3 @@
-ffmpeg -r 60 -i out$1/boardgrid%d.png -y -c:v ffv1 -qscale:v 0 simulation.avi
+#ffmpeg -r 60 -i out$1/boardgrid%d.png -y -c:v ffv1 -qscale:v 0 simulation.avi -loglevel verbose
+
+ffmpeg -r 60 -i out$1/boardgrid%d.png -y -vcodec png simulation.mov
